2bbd2f534c6a3d255632eefee8315784cface138
Александр Коноплев <aleksandrkonoplev@MacBook-Pro-Aleksandr.local>
2025-10-18 02:47:34 +0300

    646456

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://justcreatedsite.ru/api/:path*',
+      },
+    ]
+  },
   reactStrictMode: true,
   output: 'standalone',
   images: {
